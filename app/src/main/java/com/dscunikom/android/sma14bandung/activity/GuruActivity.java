3f6033a4268cd1990cd84a61956ce18d3defe9cb
helsanf <helsanfirmansyah@gmail.com>
2018-10-29 20:37:14 +0700

    Update Layout

--- app/src/main/java/com/dscunikom/android/sma14bandung/activity/GuruActivity.java
+++ app/src/main/java/com/dscunikom/android/sma14bandung/activity/GuruActivity.java
@@ -1,8 +1,16 @@
 package com.dscunikom.android.sma14bandung.activity;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.dscunikom.android.sma14bandung.R;
 import com.google.firebase.messaging.FirebaseMessaging;
@@ -10,7 +18,7 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class GuruActivity extends AppCompatActivity {
+public class GuruActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 
 
@@ -20,10 +28,65 @@ public class GuruActivity extends AppCompatActivity {
         setContentView(R.layout.activity_guru);
         ButterKnife.bind(this);
         FirebaseMessaging.getInstance().subscribeToTopic("helsan");
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @OnClick(R.id.img1)
     public void detailGuru(){
         startActivity(new Intent(this, DetailGuruActivity.class));
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+
+
+
+        int id = item.getItemId();
+        item.setChecked(true);
+
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_teacher) {
+            Intent intent = new Intent(this, GuruActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_calendar) {
+            Intent intent = new Intent(this, KalendarActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_contact) {
+            Intent intent = new Intent(this, KontakActivity.class);
+            startActivity(intent);
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
