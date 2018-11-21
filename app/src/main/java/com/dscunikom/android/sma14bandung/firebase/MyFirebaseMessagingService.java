34ccca3ff1cc1e1034007dd58487b450e0cdbbbd
Ary Sugiarto <arysugiarto10@gmail.com>
2018-11-21 17:44:47 +0700

    Update Notif Oreo

--- app/src/main/java/com/dscunikom/android/sma14bandung/firebase/MyFirebaseMessagingService.java
+++ app/src/main/java/com/dscunikom/android/sma14bandung/firebase/MyFirebaseMessagingService.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
@@ -61,6 +62,8 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     }
 
     private void sendNotification(String body , String activity ) {
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         Intent intent = new Intent();
         Intent intentNew = sendMessage(activity,intent);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intentNew,
@@ -74,8 +77,18 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent);
 
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel notificationChannel = new NotificationChannel(body, "NOTIFICATION_CHANNEL_NAME", importance);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            notificationBuilder.setChannelId(body);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+
         notificationManager.notify(1, notificationBuilder.build());
     }
     private void onMessageRecivedAcara(RemoteMessage remoteMessage){
